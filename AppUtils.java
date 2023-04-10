6212dd5fb79cfb724c340c328af84a0c48733968
HK Transfield <h.k.transfield1@gmail.com>
2023-04-10 16:53:17 +1200

    docs: link source for colour codes

--- AppUtils.java
+++ AppUtils.java
@@ -1,4 +1,5 @@
 public class AppUtils {
+    // https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_BLACK = "\u001B[30m";
     public static final String ANSI_RED = "\u001B[31m";
