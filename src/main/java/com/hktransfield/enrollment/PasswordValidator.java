95b83d261107851a431031b1946c12abd55b6cf5
HK Transfield <h.k.transfield1@gmail.com>
2023-04-25 10:05:17 +1200

    doc: add comments

--- src/main/java/com/hktransfield/enrollment/PasswordValidator.java
+++ src/main/java/com/hktransfield/enrollment/PasswordValidator.java
@@ -5,33 +5,43 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/** Represents a password validtator
+ *
+ * This utilities class provides methods to enfore various NIST password requirements you might
+ * want to implement in a password authentication based system.
+ */
 public class PasswordValidator {
     private static final int MIN_PASSWORD_LENGTH = 8;
     private static final int MAX_PASSWORD_LENGTH = 64;
 
     /**
+     * Ensures that the prospect password does not match a prospect username
      *
-     * @param username
-     * @param password
-     * @return
+     * @param username the username the user wants
+     * @param password the password the user wants
+     * @return true if the password matches the username
      */
     public static boolean matchesUsername(String username, char[] password) {
         return Arrays.equals(username.toCharArray(), password);
     }
 
     /**
+     * Imposes the NIST requirements that passwords must be a minimum length of
+     * 8 characters
      *
-     * @param password
-     * @return
+     * @param password the prospect password the user wants
+     * @return true if password does not meet minimum length
      */
     public static boolean isMinimumLength(char[] password) {
         return password.length < MIN_PASSWORD_LENGTH;
     }
 
     /**
+     * Imposes the NIST requirements that passwords not be greater than length of
+     * 64 characters
      *
-     * @param password
-     * @return
+     * @param password the prospect password the user wants
+     * @return true if password is longer than maximum length
      */
     public static boolean exceedsMaxmimumLength(char[] password) {
         return password.length > MAX_PASSWORD_LENGTH;
@@ -40,66 +50,38 @@ public class PasswordValidator {
     /**
      *
      * @param password
-     * @return
-     * @throws IOException
-     */
-    public static boolean isPasswordWeak(char[] password) throws IOException {
-        // check if password is a weak password
-        List<String> listWeakPasswords = textFileToList("weakpasswords.txt");
-
-        for(String weakPassword : listWeakPasswords)
-            if(Arrays.equals(password, weakPassword.toCharArray()))
-                return true;
-
-        return false;
-    }
-
-    /**
-     *
-     * @param password
-     * @return
-     * @throws IOException
-     */
-    public static boolean isPasswordBreached(char[] password) throws IOException {
-        // check if password is in a breached database
-        List<String> listBreachedPasswords = textFileToList("breachedpasswords.txt");
-
-        for(String breachedPassword : listBreachedPasswords)
-            if(Arrays.equals(password, breachedPassword.toCharArray()))
-                return true;
-
-        return false;
-    }
-
-    /**
-     * Converts a text file into a list of strings.
-     *
      * @param filename
      * @return
      * @throws IOException
      */
-    private static List<String> textFileToList(String filename) throws IOException {
-        // declare IO
-        ClassLoader cl = Thread.currentThread().getContextClassLoader();
-        InputStream fis = cl.getResourceAsStream(filename);
-        InputStreamReader isr = new InputStreamReader(fis);
-        BufferedReader br = new BufferedReader(isr);
+    public static boolean isPasswordKnown(char[] password, String filename) throws IOException {
 
-        // declare variables
-        List<String> listStrings = new ArrayList<String>();
-        String line = br.readLine();
+         // declare IO
+         ClassLoader cl = Thread.currentThread().getContextClassLoader();
+         InputStream fis = cl.getResourceAsStream(filename);
+         InputStreamReader isr = new InputStreamReader(fis);
+         BufferedReader br = new BufferedReader(isr);
 
-        // checking for end of file
-        while (line != null) {
-            listStrings.add(line);
-            line = br.readLine();
-        }
+         // declare variables
+         List<String> listKnownPasswords = new ArrayList<String>();
+         String line = br.readLine();
 
-        // closing bufferreader object
-        fis.close();
-        isr.close();
-        br.close();
+         // checking for end of file
+         while (line != null) {
+             listKnownPasswords.add(line);
+             line = br.readLine();
+         }
 
-        return listStrings;
+         // closing IO
+         fis.close();
+         isr.close();
+         br.close();
+
+
+        for(String knownPassword : listKnownPasswords)
+            if(Arrays.equals(password, knownPassword.toCharArray()))
+                return true;
+
+        return false;
     }
 }
--- src/main/java/com/hktransfield/enrollment/UsernameValidator.java
+++ src/main/java/com/hktransfield/enrollment/UsernameValidator.java
@@ -5,31 +5,38 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+/** Represents a username validtator
+ *
+ * This utilities class provides methods to enfore various username requirements you might
+ * want to implement in a password authentication based sysmte
+ */
 public class UsernameValidator {
     private static final String REGEX = "^[A-Za-z][A-Za-z0-9_]{7,29}$";  // regex from https://laasyasettyblog.hashnode.dev/validating-username-using-regex
 
     /**
+     * Imposes a rule that checks if a prospect username contains
+     * valid characters and matches the regex.
      *
-     * @param username
-     * @return
+     * @param username the newly created username entered by the user
+     * @return true if the username matches the required regex
      */
     public static boolean hasValidCharacters(String username) {
-        username = username.toLowerCase();
-
         return username.matches(REGEX);
     }
 
     /**
+     * Imposes a rule that checks if a prospect username contains any
+     * profanities.
      *
-     * @param username
-     * @return
+     * @param username the newly created username entered by the user
+     * @return true if the username contains profanity
      */
-    public static boolean hasSwearWords(String username) {
+    public static boolean hasProfanity(String username) {
         username = username.toLowerCase();
 
-        ProfanityFilter.loadWordFilter();
+        ProfanityFilter.loadProfanityFilter();
 
-        return ProfanityFilter.checkForBadWords(username);
+        return ProfanityFilter.checkForProfanity(username);
     }
 
     /** Represents a profanity filter
@@ -38,18 +45,18 @@ public class UsernameValidator {
     private static class ProfanityFilter {
 
         // declare class scope variables
-        static Map<String, String[]> words = new HashMap<>();
-        static int largestWordLength = 0;
+        private static Map<String, String[]> words = new HashMap<>();
+        private static int largestWordLength = 0;
 
         /**
          * Loads in the CSV file containing all swears and obscenities.
          */
-        private static void loadWordFilter() {
+        private static void loadProfanityFilter() {
             String line = "";
 
             try {
                 ClassLoader cl = Thread.currentThread().getContextClassLoader();
-                InputStream fis = cl.getResourceAsStream("Word_Filter.csv");
+                InputStream fis = cl.getResourceAsStream("profanityfilter.csv");
                 InputStreamReader isr = new InputStreamReader(fis);
                 BufferedReader br = new BufferedReader(isr);
 
@@ -57,32 +64,26 @@ public class UsernameValidator {
 
                     String[] content = null;
 
-                    try {
-                        content = line.split(",");
+                    // split profanities with words to ignore in combination with
+                    content = line.split(",");
 
-                        if(content.length == 0) continue;
+                    if(content.length == 0) continue;
 
-                        String word = content[0];
-                        String[] ignoreInCombinationWithWords = new String[]{};
+                    String word = content[0];
+                    String[] ignoreInCombinationWithWords = new String[]{};
 
-                        if(content.length > 1) {
-                            ignoreInCombinationWithWords = content[1].split("_");
-                        }
+                    if(content.length > 1) ignoreInCombinationWithWords = content[1].split("_");
+                    if(word.length() > largestWordLength) largestWordLength = word.length();
 
-                        if(word.length() > largestWordLength) {
-                            largestWordLength = word.length();
-                        }
-                        words.put(word.replaceAll(" ", ""), ignoreInCombinationWithWords);
+                    words.put(word.replaceAll(" ", ""), ignoreInCombinationWithWords);
 
-                    } catch(Exception e) {
-                        e.printStackTrace();
-                    }
                     line = br.readLine();
                 }
                 fis.close();
                 isr.close();
                 br.close();
-            } catch (IOException e) {
+
+            } catch(IOException e) {
                 e.printStackTrace();
             }
         }
@@ -90,22 +91,20 @@ public class UsernameValidator {
 
         /**
          * Iterates over a String input and checks whether a cuss word was found in a list, then checks if the word should be ignored (e.g. bass contains the word *ss).
-        * @param input
-        * @return
+         *
+        * @param input the user input to check for any profanity
+        * @return true if the input contains profanity
         */
+        private static boolean checkForProfanity(String input) {
 
-        private static boolean checkForBadWords(String input) {
+            if(input == null) return true;
 
-            if(input == null) {
-                return true;
-            }
-
-            // don't forget to remove leetspeak, probably want to move this to its own function and use regex if you want to use this
+            // remove leetspeak
             input = removeLeetSpeak(input);
 
             ArrayList<String> swearWords = new ArrayList<>();
             input = input.toLowerCase().replaceAll("[^a-zA-Z]", "");
-            input = input.replaceAll("_", " ");
+            input = input.replaceAll("_", " "); // usernames will not have spaces
 
             // iterate over each letter in the word
             for(int i = 0; i < input.length(); i++) {
@@ -113,11 +112,11 @@ public class UsernameValidator {
                 // from each letter, keep going to find bad words until either the end of the sentence is reached, or the max word length is reached.
                 for(int offset = 1; offset < (input.length()+1 - i) && offset < largestWordLength; offset++)  {
 
-                    String wordToCheck = input.substring(i, i + offset);
+                    String currWord = input.substring(i, i + offset);
 
-                    if(words.containsKey(wordToCheck)) {
+                    if(words.containsKey(currWord)) {
                         // for example, if you want to say the word bass, that should be possible.
-                        String[] ignoreCheck = words.get(wordToCheck);
+                        String[] ignoreCheck = words.get(currWord);
                         boolean ignore = false;
 
                         for(int s = 0; s < ignoreCheck.length; s++ ) {
@@ -127,28 +126,21 @@ public class UsernameValidator {
                                 break;
                             }
                         }
-
-                        if(!ignore) {
-                            swearWords.add(wordToCheck);
-                        }
+                        if(!ignore) swearWords.add(currWord);
                     }
                 }
             }
 
-            if(swearWords.size() > 0)
-                return false;
-
-            return true;
+            return swearWords.size() > 0;
         }
 
         /**
-         * Ensures that users are not able to bypass
-         * the rule that does not allow the use of
-         * swear words when creating a username by
+         * Ensures that users are not able to bypass the rule that does not
+         * allow the use of swear words when creating a username by
          * substituting numbers or symbols for letters.
          *
-         * @param input
-         * @return
+         * @param input the user input to check for leetspeak
+         * @return the input with all substitute numbers or symbols replaced
          */
         private static String removeLeetSpeak(String input) {
             input = input.replaceAll("1","i");
